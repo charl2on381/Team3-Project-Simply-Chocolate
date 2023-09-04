e3fec2910795d5ebd1c9aef5f2bca01c74b9d828
charl2on381 <charl2on381@gmail.com>
2023-09-04 19:09:26 +0800

    Update

--- js/review-swiper.js
+++ js/review-swiper.js
@@ -18,54 +18,54 @@
 //   },
 // });
 
-// const swiper = new Swiper('.swiper-rev', {
-  // Default parameters
-//   slidesPerView: 1,
-//   spaceBetween: 28,
-//   loop: true,
-//   // Responsive breakpoints
-//   breakpoints: {
-//     // when window width is >= 375px
-//     375: {
-//       slidesPerView: 1,
-//       spaceBetween: 28,
-//     },
-//     // when window width is >= 768px
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 28,
-//     },
-//     // when window width is >= 1200px
-//     1200: {
-//       slidesPerView: 3,
-//       spaceBetween: 28,
-//     },
-//   },
-//   // Pagination
-//   pagination: {
-//     el: '.swiper-pagination-review',
-//     clickable: true,
-//   },
-// });
-  const swiper = new Swiper('.swiper-rev', {
+const swiper = new Swiper('.swiper-rev', {
+  Default parameters
   slidesPerView: 1,
-  // slidesPerGroup: 2,
   spaceBetween: 28,
-  // centerInsufficientSlides: true,
-  // centeredSlides: true,
-  // centeredSlidesBounds: true,
+  loop: true,
+  // Responsive breakpoints
   breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 28,
+    },
+    // when window width is >= 768px
     768: {
       slidesPerView: 2,
-      spaceBetween: 16,
+      spaceBetween: 28,
     },
+    // when window width is >= 1200px
     1200: {
       slidesPerView: 3,
       spaceBetween: 28,
     },
   },
+  // Pagination
   pagination: {
     el: '.swiper-pagination-review',
     clickable: true,
   },
 });
+//   const swiper = new Swiper('.swiper-rev', {
+//   slidesPerView: 1,
+//   // slidesPerGroup: 2,
+//   spaceBetween: 28,
+//   // centerInsufficientSlides: true,
+//   // centeredSlides: true,
+//   // centeredSlidesBounds: true,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 16,
+//     },
+//     1200: {
+//       slidesPerView: 3,
+//       spaceBetween: 28,
+//     },
+//   },
+//   pagination: {
+//     el: '.swiper-pagination-review',
+//     clickable: true,
+//   },
+// });
