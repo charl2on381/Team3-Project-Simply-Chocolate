bf21b5ec8684c78e3a4f152593ce53917b7a63fa
charl2on381 <charl2on381@gmail.com>
2023-09-04 18:54:10 +0800

    Update

--- js/review-swiper.js
+++ js/review-swiper.js
@@ -1,47 +1,47 @@
-// var swiper = new Swiper('.swiper', {
-//   slidesPerView: 1,
-//   spaceBetween: 0,
-//   loop: true,
-//   breakpoints: {
-//     375: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//     },
-//     // when window width is >= 768px
-//     768: {
-//       slidesPerView: 2.46,
-//       spaceBetween: 19,
-//     },
-//     // when window width is >= 1200px
-//     1200: {
-//       slidesPerView: 4,
-//       spaceBetween: 19,
-//     },
-//   },
+// let sliderToShow = 1;
+// if (document.body.clientWidth >= 768 && document.body.clientWidth < 1200) {
+//   sliderToShow = 2;
+// }
+// if (document.body.clientWidth >= 1200) {
+//   sliderToShow = 3;
+// }
+
+// let spaceBetweenSliders = document.body.clientWidth < 768 ? 0 : 28;
+
+// var swiper = new Swiper('.swiper-rew', {
+//   slidesPerView: sliderToShow,
+//   spaceBetween: spaceBetweenSliders,
+
 //   pagination: {
-//     el: '.swiper-pagination',
+//     el: '.swiper-pagination-review',
 //     clickable: true,
 //   },
 // });
 
-// Swiper for reviews
-var reviewsSwiper = new Swiper('.swiper-review', {
+const swiper = new Swiper('.swiper-rew', {
+  // Default parameters
   slidesPerView: 1,
-  // slidesPerGroup: 2,
   spaceBetween: 28,
-  // centerInsufficientSlides: true,
-  // centeredSlides: true,
-  // centeredSlidesBounds: true,
+  loop: true,
+  // Responsive breakpoints
   breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 28,
+    },
+    // when window width is >= 768px
     768: {
       slidesPerView: 2,
-      spaceBetween: 16,
+      spaceBetween: 28,
     },
+    // when window width is >= 1200px
     1200: {
       slidesPerView: 3,
       spaceBetween: 28,
     },
   },
+  // Pagination
   pagination: {
     el: '.swiper-pagination-review',
     clickable: true,
