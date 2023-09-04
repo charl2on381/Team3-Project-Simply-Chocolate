5e84fd13d69613e13fdf9047902fd0ce1abc4b19
charl2on381 <charl2on381@gmail.com>
2023-09-04 18:34:54 +0800

    Update

--- js/review-swiper.js
+++ js/review-swiper.js
@@ -1,21 +1,45 @@
-var swiper = new Swiper('.swiper', {
+// var swiper = new Swiper('.swiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   loop: true,
+//   breakpoints: {
+//     375: {
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//     },
+//     // when window width is >= 768px
+//     768: {
+//       slidesPerView: 2.46,
+//       spaceBetween: 19,
+//     },
+//     // when window width is >= 1200px
+//     1200: {
+//       slidesPerView: 4,
+//       spaceBetween: 19,
+//     },
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
+// Swiper for reviews
+const reviewsSwiper = new Swiper('.swiper-review', {
   slidesPerView: 1,
-  spaceBetween: 0,
-  loop: true,
+  // slidesPerGroup: 2,
+  spaceBetween: 28,
+  // centerInsufficientSlides: true,
+  // centeredSlides: true,
+  // centeredSlidesBounds: true,
   breakpoints: {
-    375: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-    },
-    // when window width is >= 768px
     768: {
-      slidesPerView: 2.46,
-      spaceBetween: 19,
+      slidesPerView: 2,
+      spaceBetween: 16,
     },
-    // when window width is >= 1200px
     1200: {
-      slidesPerView: 4,
-      spaceBetween: 19,
+      slidesPerView: 3,
+      spaceBetween: 28,
     },
   },
   pagination: {
